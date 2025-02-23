425d4528c1411188be20c9bc3dc6b4d5b1f13064
Munoz Daniel <dmunoz@micracel.com.ar>
2025-02-23 10:27:12 -0300

    obtener todos con paginacion

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,8 +7,10 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
@@ -16,18 +18,22 @@ export class ProductsController {
   constructor(
     @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
   ) {}
-  @Get()
-  getProducts() {
-    return this.productsClient.send({ cmd: 'get-products' }, {});
-  }
+
   @Post()
   createProduct() {
     return 'Create product';
   }
+
+  @Get()
+  getProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send({ cmd: 'get-products' }, paginationDto);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Product ${id}`;
   }
+
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
     return `Delete Product ${id}`;
